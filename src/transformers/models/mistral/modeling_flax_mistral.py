fca6f89106f9176410dc60be68301ea236b03ad4
devngho <yjh135908@gmail.com>
2024-09-17 02:41:50 +0900

    mistral head_dim correction

--- src/transformers/models/mistral/modeling_flax_mistral.py
+++ src/transformers/models/mistral/modeling_flax_mistral.py
@@ -232,11 +232,7 @@ class FlaxMistralAttention(nn.Module):
         self.max_position_embeddings = config.max_position_embeddings
         self.attention_softmax_in_fp32 = self.dtype is not jnp.float32
         self.rope_theta = config.rope_theta
-        if (self.head_dim * self.num_heads) != self.hidden_size:
-            raise ValueError(
-                f"hidden_size must be divisible by num_heads (got `hidden_size`: {self.hidden_size}"
-                f" and `num_heads`: {self.num_heads})."
-            )
+
         self.q_proj = nn.Dense(self.num_heads * self.head_dim, use_bias=False, dtype=self.dtype)
         self.k_proj = nn.Dense(self.num_key_value_heads * self.head_dim, use_bias=False, dtype=self.dtype)
         self.v_proj = nn.Dense(self.num_key_value_heads * self.head_dim, use_bias=False, dtype=self.dtype)
